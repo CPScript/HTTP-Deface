c76611c9d7789ad2ec8d2f75d8ee51a4477f9c49
Lovely <83523587+CPScript@users.noreply.github.com>
2023-10-18 14:10:42 -0400

    Update ace.py

--- ace.py
+++ ace.py
@@ -59,7 +59,7 @@ def main(__bn__):
    print(__bn__)
    while True:
       try:
-         a = x("Enter your script deface name: ")
+         a = x("Enter your site List: ")
          if not os.path.isfile(a):
             print("file '%s' not found"%(a))
             continue
