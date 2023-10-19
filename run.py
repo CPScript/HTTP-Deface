22ed4b289497d2528ed36b296e4c315ff1a2ac51
Lovely <83523587+CPScript@users.noreply.github.com>
2023-10-19 02:47:49 +0000

    Create run.py

---
+++ run.py
@@ -0,0 +1,38 @@
+from scripts.sprint import sprint
+from scripts.colors import ran, y, r, g, c
+from subprocess import call
+import time
+import os
+from os import system
+os.system('clear')
+
+# welcome
+sprint(f"Welcome to Ace... your personal Web Defacer")
+sprint(f"{r}Please use a {g}VPN {r}for your {g}safety")
+time.sleep(5)
+print("Have fun!!!")
+time.sleep(0.5)
+os.system('clear')
+
+# selection
+print("To get started please select a version of Ace:")
+hoice = input("")
+
+
+if choice == "1":
+    os.system('clear')
+    time.sleep(1)
+    sprint(f"You have selected AceV1")
+    print("Loading...")
+    time.sleep(2)
+    os.system('clear')
+    call(["python", "version/aceV1.py"])
+
+if choice == "2":
+    os.system('clear')
+    time.sleep(1)
+    sprint(f"You have selected AceV2")
+    print("Loading...")
+    time.sleep(2)
+    os.system('clear')
+    call(["python", "version/aceV2.py"])
